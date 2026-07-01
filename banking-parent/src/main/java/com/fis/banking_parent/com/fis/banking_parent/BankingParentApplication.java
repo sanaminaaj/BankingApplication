b7ffdd9d243c4e6bcb14cd97d7f4d3a1e10e4d16
sana <sanafarooqui2002@gmail.com>
2026-07-01 18:23:48 +0530

    commit us-004

---
+++ banking-parent/src/main/java/com/fis/banking_parent/com/fis/banking_parent/BankingParentApplication.java
@@ -0,0 +1,13 @@
+package com.fis.banking_parent.com.fis.banking_parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingParentApplication.class, args);
+	}
+
+}
---
+++ banking-parent/src/test/java/com/fis/banking_parent/BankingParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fis.banking_parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
